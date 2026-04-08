341e37ec25ced4dfdc37f5dfedf1dee06a0a0c3b
Richard van Schagen <richard@routerwrt.org>
2026-04-08 21:38:22 +0200

    crypto: mtk-aes: add descriptor definitions and ring allocation
    Add TX/RX descriptor definitions based on the vendor driver and
    introduce basic ring structures.
    
    Allocate coherent memory for TX and RX descriptor rings and
    initialize ring indices. This prepares the driver for DMA-based
    request submission.
    
    Signed-off-by: Richard van Schagen <richard@routerwrt.org>

--- drivers/crypto/mtk_aes.c
+++ drivers/crypto/mtk_aes.c
@@ -5,6 +5,7 @@
  */
 
 #include <linux/clk.h>
+#include <linux/dma-mapping.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
@@ -12,10 +13,36 @@
 
 #include "mtk_aes.h"
 
+static int mtk_aes_alloc_desc(struct mtk_aes_priv *mtk)
+{
+	mtk->tx_ring.base = dmam_alloc_coherent(mtk->dev,
+			sizeof(struct mtk_aes_tx_desc) *
+			MTK_AES_RING_SIZE,
+			&mtk->tx_ring.base_dma, GFP_KERNEL);
+
+	if (!mtk->tx_ring.base)
+		return -ENOMEM;
+
+	mtk->rx_ring.base = dmam_alloc_coherent(mtk->dev,
+			sizeof(struct mtk_aes_rx_desc) *
+			MTK_AES_RING_SIZE,
+			&mtk->rx_ring.base_dma, GFP_KERNEL);
+
+	if (!mtk->rx_ring.base)
+		return -ENOMEM;
+
+	mtk->tx_ring.cpu_idx = 0;
+	mtk->tx_ring.dma_idx = 0;
+	mtk->rx_ring.cpu_idx = 0;
+	mtk->rx_ring.dma_idx = 0;
+
+	return 0;
+}
+
 static int mtk_aes_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
-	struct mtk_aes *mtk;
+	struct mtk_aes_priv *mtk;
 	struct resource *res;
 	int err;
 
@@ -62,7 +89,11 @@ static int mtk_aes_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, mtk);
 
-	/* TODO: rings, IRQ, engine */
+	err = mtk_aes_alloc_desc(mtk);
+	if (err)
+		goto err_clk;
+
+	/* TODO: IRQ, engine */
 
 	return 0;
 
@@ -73,7 +104,7 @@ err_clk:
 
 static void mtk_aes_remove(struct platform_device *pdev)
 {
-	struct mtk_aes *mtk = platform_get_drvdata(pdev);
+	struct mtk_aes_priv *mtk = platform_get_drvdata(pdev);
 
 	if (mtk->rst)
 		reset_control_assert(mtk->rst);
@@ -99,6 +130,6 @@ static struct platform_driver mt7628_aes_driver = {
 
 module_platform_driver(mt7628_aes_driver);
 
-MODULE_AUTHOR("Richard van Schagen <vschagen@cs.com>");
+MODULE_AUTHOR("Richard van Schagen <richard@routerwrt.org>");
 MODULE_DESCRIPTION("Mediatek MT7628 AES Crypto hardware driver");
 MODULE_LICENSE("GPL");
--- drivers/crypto/mtk_aes.h
+++ drivers/crypto/mtk_aes.h
@@ -2,12 +2,80 @@
 #ifndef __MTK_AES_H
 #define __MTK_AES_H
 
-struct mtk_aes {
+#define MTK_AES_RING_SIZE		128
+
+/* Power-of-2 masks */
+#define MTK_AES_RING_MASK		(MTK_AES_RING_SIZE - 1)
+
+/*
+ * AES AES_RX Descriptor Format define
+ */
+
+#define RX2_DMA_SDL0_GET(_x)		(((_x) >> 16) & 0x3fff)
+#define RX2_DMA_SDL0_SET(_x)		(((_x) & 0x3fff) << 16)
+#define RX2_DMA_LS0			BIT(30)
+#define RX2_DMA_DONE			BIT(31)
+
+#define RX4_DMA_ENC			BIT(2)
+#define RX4_DMA_UDV			BIT(3)
+#define RX4_DMA_CBC			BIT(4)
+#define RX4_DMA_IVR			BIT(5)
+#define RX4_DMA_KIU			BIT(6)
+
+struct mtk_aes_rx_desc {
+	unsigned int			SDP0;
+	unsigned int			rxd_info2;
+	unsigned int			user_data;
+	unsigned int			rxd_info4;
+	unsigned int			IV[4];
+}__aligned(32);
+
+/*
+ * AES AES_TX Descriptor Format define
+ */
+
+#define TX2_DMA_SDL1_SET(_x)		((_x) & 0x3fff)
+#define TX2_DMA_LS1			BIT(14)
+#define TX2_DMA_SDL0_SET(_x)		(((_x) & 0x3fff) << 16)
+#define TX2_DMA_LS0			BIT(30)
+#define TX2_DMA_DONE			BIT(31)
+
+#define TX4_DMA_ENC			BIT(2)
+#define TX4_DMA_UDV			BIT(3)
+#define TX4_DMA_CBC			BIT(4)
+#define TX4_DMA_IVR			BIT(5)
+#define TX4_DMA_KIU			BIT(6)
+
+#define TX4_DMA_AES_128			0
+#define TX4_DMA_AES_192			1
+#define TX4_DMA_AES_256			2
+
+struct mtk_aes_tx_desc {
+	unsigned int			SDP0;
+	unsigned int			txd_info2;
+	unsigned int			SDP1;
+	unsigned int			txd_info4;
+	unsigned int			IV[4];
+}__aligned(32);
+
+
+struct mtk_aes_ring {
+	void				*base;
+	dma_addr_t			base_dma;
+	u16				cpu_idx;
+	u16				dma_idx;
+};
+
+struct mtk_aes_priv {
 	void __iomem			*base;
 	struct device			*dev;
 
 	struct clk			*clk;
 	struct reset_control		*rst;
 	int				irq;
+
+	struct mtk_aes_ring		tx_ring;
+	struct mtk_aes_ring		rx_ring;
 };
-#endif /* __RALINK_FE_H */
+
+#endif /* __MTK_AES_H */
